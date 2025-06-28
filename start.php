4f15d8f8d8dd014f2a9aa1c4fbc6aa05d511fd8e
PirvuStefan <silverbench24@gmail.com>
2025-06-28 20:25:23 +0300

    pare ok

--- start.php
+++ start.php
@@ -38,20 +38,51 @@ function get_random_questionnaire() {
     return null;
 }
 
+
+
 class Questionnaire {
     public $id;
     public array $questions = []; 
+
+    public function __construct($id) {
+        $this->id = $id;
+    }
+
+    public function add_question(Question $question) {
+        $this->questions[] = $question;
+    }
 }
 
 class Question {
+    
     public int $id;
-    public string $desciption;
+        public string $description;
     public string $answers1;
     public string $answers2;   
     public string $answers3;
     public string $answers1_correct;
     public string $answers2_correct;
     public string $answers3_correct;
+
+    public function __construct(
+        int $id,
+        string $description,
+        string $answers1,
+        string $answers2,
+        string $answers3,
+        string $answers1_correct,
+        string $answers2_correct,
+        string $answers3_correct
+    ) {
+        $this->id = $id;
+        $this->description = $description;
+        $this->answers1 = $answers1;
+        $this->answers2 = $answers2;
+        $this->answers3 = $answers3;
+        $this->answers1_correct = $answers1_correct;
+        $this->answers2_correct = $answers2_correct;
+        $this->answers3_correct = $answers3_correct;
+    }
 }// firecare intrebare are un id, o descriere si 3 raspunsuri, fiecare cu un camp de corectitudine
 
 ?>
