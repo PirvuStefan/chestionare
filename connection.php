08b3d9d67840080a02de77bc090101821507dc15
pirvu <117602013+PirvuStefan@users.noreply.github.com>
2025-07-19 17:59:06 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "robestnew_chestionareutil";
-$password = "mgNK?y!cYvqe";
-$dbname = "robestnew_chestionare";
+$username = "root";
+$password = "";
+$dbname = "table";
 $conn = new mysqli($servername, $username, $password, $dbname, 3306);
 if (!$conn) {
     die('Connection failed: ' . mysqli_connect_error());
